4d30eb5ffbc38176ea1a53c56a4475e2d4afdb19
dmnitprof <54173914+dmnitprof@users.noreply.github.com>
2020-03-24 06:38:37 +0300

    add navbar button collapse

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,10 @@ export const Navbar = () => (
     <div className="navbar-brand">
     блог Прогера
     </div>
-
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav">
       <li className="nav-item">
         <NavLink
@@ -34,5 +37,6 @@ export const Navbar = () => (
         </NavLink>
       </li>
     </ul>
+    </div>
   </nav>
 )
